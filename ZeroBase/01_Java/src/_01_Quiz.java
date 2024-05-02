7eef5cbcdcd65884911ec904eca212860d21b641
doheeing <doheeehan@naver.com>
2024-05-03 01:01:14 +0900

    java 기초 1일차

---
+++ ZeroBase/01_Java/src/_01_Quiz.java
@@ -0,0 +1,10 @@
+public class _01_Quiz {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 1; i <= 9; i++) {
+            for (j = 1; j <= 9; j++) {
+                System.out.println(i + "x" + j + "=" + (i * j));
+            }
+        }
+    }
+}
