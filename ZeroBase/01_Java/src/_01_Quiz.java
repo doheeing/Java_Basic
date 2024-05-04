e88c1f83adb653d31563fd489ee5b9fb982b6a61
doheeing <doheeehan@naver.com>
2024-05-04 19:33:44 +0900

    java 공부 4일차

--- ZeroBase/01_Java/src/_01_Quiz.java
+++ ZeroBase/01_Java/src/_01_Quiz.java
@@ -1,6 +1,7 @@
 public class _01_Quiz {
     public static void main(String[] args) {
         int i, j;
+        System.out.println("[구구단 출력]");
         for (i = 1; i <= 9; i++) {
             for (j = 1; j <= 9; j++) {
                 System.out.println(i + "x" + j + "=" + (i * j));
---
+++ ZeroBase/01_Java/src/_03_Java.java
@@ -0,0 +1,60 @@
+// Java 프로그래밍 - 여러가지 연산자_2
+
+public class _03_Java {
+    public static void main(String[] args) {
+
+//      1. 비트 논리 연산자
+        System.out.println("== 비트 논리 연산자 ==");
+//      1-1. AND 연산자 (&)
+        int num1 = 5;
+        int num2 = 3;
+        int result = 0;
+
+        result = num1 & num2;
+        System.out.println(result);
+        System.out.println(Integer.toBinaryString(num1));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num2)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+
+
+//      1-2. OR 연산자 (|)
+        result = num1 | num2;
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num2)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+
+
+//      1-3. XOR 연산자 (^)
+        result = num1 ^ num2;
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num2)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+
+//      1-4. 반전 연산자 (~)
+        result = ~num1;
+        System.out.println("result = " + result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%s\n", Integer.toBinaryString(result));
+
+//      2. 비트 이동 연산자
+        System.out.println("== 비트 이동 연산자 ==");
+        int numA = 3;
+        result = numA << 1;
+        System.out.println("result = "+result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(numA)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+
+
+//      2-1. << 연산자
+
+
+//      2-2. >> 연산자
+
+
+//      2-3. >>> 연산자
+
+
+    }
+
+}
